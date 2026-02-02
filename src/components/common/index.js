9c6b2716ee6e50d3f1ff9da8b9c7bbd3e275bc4e
Abdullah Al Noman Khan <mdnomanahamed22@gmail.com>
2026-02-02 09:37:42 +0600

    Add .\src\components\common\index.js

---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+// Common Components exports
+export { default as DestinationCard } from './DestinationCard';
+export { default as ServiceCard } from './ServiceCard';
+export { default as TestimonialCard } from './TestimonialCard';
+export { default as CounsellorCard } from './CounsellorCard';
