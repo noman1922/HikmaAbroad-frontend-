5180a2476bca21576f01524df8d3c26ce269bb38
Abdullah Al Noman Khan <mdnomanahamed22@gmail.com>
2026-02-07 07:39:40 +0600

    Add .\src\components\UI\SkeletonCard.jsx

---
+++ src/components/UI/SkeletonCard.jsx
@@ -0,0 +1,14 @@
+function SkeletonCard() {
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden animate-pulse">
+      <div className="h-48 bg-gray-300"></div>
+      <div className="p-6">
+        <div className="h-6 bg-gray-300 rounded mb-4"></div>
+        <div className="h-4 bg-gray-300 rounded mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default SkeletonCard;
